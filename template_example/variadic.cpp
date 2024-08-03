66d107e8372d91e9ffe76658d2f117f61eb26e1d
lsm <487005831@qq.com>
2024-08-04 01:50:26 +0800

    模板展开

---
+++ template_example/variadic.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+template <typename... T>
+void sayHello(T... args)
+{
+    std::cout << "参数数量=" << sizeof...(args) << std::endl;
+    std::cout << "参数数量=" << sizeof...(T) << std::endl;
+}
+
+void sayHello2() 
+{
+    // 递归终止
+}
+
+// 这种方式比较容易展开
+template <typename T, typename... U>
+void sayHello2(const T &frist, const U&... otherargs) 
+{
+    std::cout << "收到的参数值=" << frist << std::endl;
+    // 递归展开
+    sayHello2(otherargs...);
+}
+
+int main()
+{
+    std::cout << "123" << std::endl;
+    sayHello<>();
+    sayHello2<int, double, float>(1, 1.2, 1.3f);
+    return 0;
+}
